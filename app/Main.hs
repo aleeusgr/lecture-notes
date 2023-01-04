cd5b11b860a291c2a3cf5d8b8c43bb18779c5f4e
50^2 <alexeusgr@gmail.com>
2023-01-04 09:43:53 +0200

    minimal q

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,16 @@ module Main where
 
 import qualified MyLib (someFunc)
 
+-- this should be a Set; 
+x = "abcde"
+-- quantifier on set will work differently
+qunatifier xs = zip xs [1..]
+
+--selection may be as element x where second x is the element with largest value of quantifier 
+--selection ys = 
 
 
 main :: IO ()
 main = do
-  print (zip "abcde" [1..])
+  print (qunatifier x)
   MyLib.someFunc
